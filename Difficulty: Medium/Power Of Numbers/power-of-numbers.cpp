9d4e8bea09c9af0f44e377a6cf40bbd832178460
Aryan Sharma <aryansharma7341.as@gmail.com>
2026-03-31 00:51:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Power Of Numbers/power-of-numbers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int reverseExponentiation(int n) {
+        // code here
+        int i,x=0,y=n,z=n;
+        if(n>=10)
+        {        
+            while(z>0)
+            {
+                x=x*10+z%10;
+                z=z/10;
+            }
+            for(i=1;i<=x;i++)
+            {
+                if(x>=2)
+                {
+                    z=n*n;
+                }
+                else
+                {
+                    z=n*1;
+                }
+            }
+            return z;
+        }
+        for(i=2;i<=n;i++)
+        {
+            y=y*n;
+        }
+        return y;
+    }
+};
